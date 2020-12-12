6f7894733d5e69d0a6e755cd5a72fbe02e9b87a5
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2020-12-12 14:08:34 +1100

    initial commit

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "Australia is the largest country in Oceania and the world's __ largest country by total area.",
+    answers: ["3rd", "4th", "5th", "6th"],
+    correct: "6th",
+    questionId: "099099"
+  },
+  {
+    question:
+      "__ was the world's fifth largest economy by nominal GDP in 2020.",
+    answers: ["France", "United Kingdom", "India", "Germany"],
+    correct: "United Kingdom",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Which airport was the world's busiest by passenger traffic in 2019?",
+    answers: ["Beijing Capital International Airport", "Hartsfield–Jackson Atlanta International Airport", "Dubai International Airport", "Shanghai Pudong International Airport"],
+    correct: "Hartsfield–Jackson Atlanta International Airport",
+    questionId: "267908"
+  },
+  {
+    question: "As of December 2020, how many member states does the United Nations have?",
+    answers: [
+      "190",
+      "191",
+      "193",
+      "210"
+    ],
+    correct: "1993",
+    questionId: "333247"
+  },
+  {
+    question: "The Isle of Man, also known as Mann, is a self-governing British ____ situated in the Irish Sea between Great Britain and Ireland.",
+    answers: ["Overseas Territory", "Colony", "Protectorate", "Crown Dependency"],
+    correct: "Crown Dependency",
+    questionId: "496293"
+  },
+  {
+    question:
+      "The Treaty of ____ is an international agreement that amends the two treaties which form the constitutional basis of the European Union.",
+    answers: [
+      "Lisbon",
+      "Madrid",
+      "London",
+      "Brussels"
+    ],
+    correct: "Lisbon",
+    questionId: "588909"
+  },
+  {
+    question:
+      'The ____ Area is an area comprising 26 European countries that have officially abolished all passport and all other border control at their mutual borders.',
+    answers: ["Schengen", "Warsaw", "Rome", "Vienna"],
+    correct: "Schengen",
+    questionId: "648452"
+  },
+  {
+    question: "Which country is the Asia-Pacific Economic Cooperation headquartered at?",
+    answers: ["Japan", "Philippines", "Singapore", "China"],
+    correct: "Singapore",
+    questionId: "786649"
+  },
+  {
+    question:
+      "The Soviet Union collapsed in ____ .",
+    answers: ["1981", "1985", "1988", "1991"],
+    correct: "1991",
+    questionId: "839754"
+  },
+  {
+    question:
+      "____ gained independence from Italy and the United Kingdom in 1951.",
+    answers: [
+      "Egypt",
+      "Eritea",
+      "Libya",
+      "Tunisia"
+    ],
+    correct: "Libya",
+    questionId: "98390"
+  },
+  {
+    question: "Which of the following city is not in the top 10 of the Global Financial Centers Index as of September 2020?",
+    answers: ["Singapore", "Frankfurt", "San Francisco", "Shenzhen"],
+    correct: "Frankfurt",
+    questionId: "1071006"
+  },
+  {
+    question: "The International Monetary Fund was founded in 1944 at the _____ Conference.",
+    answers: ["Bretton Woods", "Manchester", "Copenhagen", "Adelaide"],
+    correct: "Bretton Woods",
+    questionId: "1174154"
+  },
+  {
+    question: "How many prefectures are there in Japan?",
+    answers: ["40", "47", "51", "63"],
+    correct: "47",
+    questionId: "1226535"
+  },
+  {
+    question: "____ is the world's most populous country (population/km2) as of December 2020.",
+    answers: ["Bangladesh", "Netherlands", "India", "South Korea"],
+    correct: "Bangladesh",
+    questionId: "1310938"
+  },
+  {
+    question: "Which of the following country recorded the largest real gross domestic product growth rate in 2019?",
+    answers: ["Nepal", "People's Republic of China", "South Sudan", "Ethiopia"],
+    correct: "South Sudan",
+    questionId: "1436873"
+  },
+  {
+    question: "As of December 2020, ____ is the most recent sovereign state or country with widespread recognition.",
+    answers: ["South Sudan", "East Timor", "Kosovo", "Australia"],
+    correct: "South Sudan",
+    questionId: "1515110"
+  },
+  {
+    question: "How many states does Australia have?",
+    answers: ["5", "6", "7", "8"],
+    correct: "6",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "In which year was the capital city of Australia officially named Canberra?",
+    answers: ["1905", "1910", "1913", "1920"],
+    correct: "1913",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "There are __ cities with a population over 5 million people according to the United Nations 2018 estimates.",
+    answers: ["54", "66", "78", "81"],
+    correct: "81",
+    questionId: "1822532"
+  },
+  {
+    question: "Which US president was in office between 1977 and 1981?",
+    answers: [
+      "Richard Nixon",
+      "Jimmy Carter",
+      "Bill Clinton",
+      "Gerald Ford"
+    ],
+    correct: "Jimmy Carter",
+    questionId: "195075"
+  },
+  {
+    question: "As of December 2020, the Group of 7 is an intergovernmental organization consisting of France, Germany, Italy, Japan, the United Kingdom, the United States and _______ ?",
+    answers: ["People's Republic of China", "Australia", "Canada", "Russia"],
+    correct: "Canada",
+    questionId: "2019778"
+  },
+  {
+    question: "The _______ is headquartered in Geneva, Switzerland, with six semi-autonomous regional offices and 150 field offices worldwide.",
+    answers: ["World Health Organization", "World Trade Organization", "UNESCO", "World Tourism Organization"],
+    correct: "World Health Organization",
+    questionId: "2134343"
+  },
+  {
+    question: "The Food and Agriculture Organization (FAO) was established in 1945 and its headquarter is in ____ .",
+    answers: [
+      "France",
+      "Portugal",
+      "Italy",
+      "Belgium"
+    ],
+    correct: "Italy",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "Canada is a federation composed of ____ provinces and three territories.",
+    answers: ["10", "15", "21", "31"],
+    correct: "10",
+    questionId: "2379831"
+  },
+  {
+    question: "Which of the following country ranked No.1 in 2012-2019 Corruption Perceptions Index Table? (i.e. least corrupted)",
+    answers: ["New Zealand", "Singapore", "Finland", "Denmark"],
+    correct: "Denmark",
+    questionId: "2426418"
+  },
+  {
+    question: "The capital city of the Bahamas is:",
+    answers: ["Manama", "Nassau", "Tirana", "Male"],
+    correct: "Nassau",
+    questionId: "2510086"
+  },
+  {
+    question: "The capital city of Angola is:",
+    answers: [
+      "Yerevan",
+      "Dhaka",
+      "Luanda",
+      "Belmopan"
+    ],
+    correct: "Luanda",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "The capital city of the Trinidad and Tobago is Port of ______ .",
+    answers: ["Portugal", "Brazil", "England", "Spain"],
+    correct: "Spain",
+    questionId: "2796884"
+  },
+  {
+    question: "Which of the following is NOT a city in South Africa?",
+    answers: ["Pretoria", "Cape Town", "Bloemfontein", "Juba"],
+    correct: "Juba",
+    questionId: "2838900"
+  },
+  {
+    question: 'The capital city of Guatemala is the _______ City.',
+    answers: ["Guatemala", "St. George's", "Dili", "Benjul"],
+    correct: "Guatemala",
+    questionId: "298060"
+  },
+  {
+    question: "The front side of the 1,000 Japanese yen note shows a portrait of Hideyo Noguchi, who was a _______ . ",
+    answers: ["art historian", "bacteriologist", "diplomat", "marine biologist"],
+    correct: "bacteriologist",
+    questionId: "3096579"
+  },
+  {
+    question: "Pablo Picasso was born on 25 October 1881 in _____ .",
+    answers: [
+      "France",
+      "Portugal",
+      "Spain",
+      "Monaco"
+    ],
+    correct: "Spain",
+    questionId: "3182461"
+  },
+  {
+    question: "The world's largest palace is the Palace of the Parliament which is located in ____________ .",
+    answers: [
+      "United Kingdom",
+      "Romania",
+      "Russia",
+      "France"
+    ],
+    correct: "Romania",
+    questionId: "3239112"
+  },
+  {
+    question: "Which of the following country does NOT consitute more than 1.5% of the world population as of December 2020?",
+    answers: ["Pakistan", "Russia", "Egypt", "Japan"],
+    correct: "Egypt",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'What background color does the Coat of Arms of the Vatican City have?',
+    answers: ["Yellow", "White", "Black", "Red"],
+    correct: "Red",
+    questionId: "3410327"
+  },
+  {
+    question: "As of 2019, Vatican City had a total population of ______ including 453 residents.",
+    answers: ["653", "825", "1,232", "2,329"],
+    correct: "825",
+    questionId: "3542596"
+  },
+  {
+    question: "The city of ______ was the 3rd Japanese city to pass 2 million people in 1970.",
+    answers: ["Osaka", "Nagoya", "Kawasaki", "Yokohama"],
+    correct: "Yokohama",
+    questionId: "3667517"
+  },
+  {
+    question: "As of 2010, _________ was the Japan's 4th largest city by population.",
+    answers: [
+      "Nagoya",
+      "Sapporo",
+      "Osaka",
+      "Fukuoka"
+    ],
+    correct: "Nagoya",
+    questionId: "3791672"
+  },
+  {
+    question: "The first ever FIFA World Cup took place in 1930 in _______ .",
+    answers: ["Brazil", "Argentina", "Uruguay", "Peru"],
+    correct: "Uruguay",
+    questionId: "3893585"
+  },
+  {
+    question: 'Belgium, offically the ______ of Belgium, is a country in Western Europe.',
+    answers: ["Kingdom", "Republic", "Federation", "Free State"],
+    correct: "Kindgom",
+    questionId: "3913430"
+  },
+  {
+    question: "Luxembourg, officially the ________ of Luxemburg, is a landlocked country in Western Europe.",
+    answers: ["Republic", "Grand Duchy", "City", "Royal State"],
+    correct: "Grand Duchy",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "The flag of Palau has two colors: Blue and _______. ",
+    answers: ["White", "Red", "Yellow", "Green"],
+    correct: "Yellow",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "Tonga, officially the _______ of Tonga, is a Polynesian sovereign state and archipelago comprising 169 islands.",
+    answers: ["Commonwealth", "Republic", "Kingdom", "United States"],
+    correct: "Kindgom",
+    questionId: "4235063"
+  },
+  {
+    question: "What color is the 5 Fijian dollar note as of December 2020?",
+    answers: ["Orange", "Purple", "Blue", "Green"],
+    correct: "Green",
+    questionId: "4321002"
+  },
+  {
+    question: "South Africa, officially the __________ of South Africa, is the southernmost country in Africa with over 59 million people.",
+    answers: [
+      "Federation",
+      "United States",
+      "Republic",
+      "Free State"
+    ],
+    correct: "Republic",
+    questionId: "4442286"
+  },
+  {
+    question: "Australia's island state of Tasmania is named after the _____ explorer Abel Tasman.",
+    answers: [
+      "English",
+      "Dutch",
+      "Scottish",
+      "French"
+    ],
+    correct: "Dutch",
+    questionId: "4564430"
+  },
+  {
+    question: "________ is the capital of Scotland and one of its 32 council areas.",
+    answers: ["Edinburgh", "Glasgow", "Aberdeen", "Dundee"],
+    correct: "Edinburgh",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "Which country, not including Japan, has the most people of japanese decent?",
+    answers: ["Brazil", "China", "South Korea", "United States of America"],
+    correct: "Brazil",
+    questionId: "4795960"
+  },
+  {
+    question: "Elizabeth II is Queen of the United Kingdom and ___ other Commonwealth realms as of December 2020.",
+    answers: ["8", "15", "32", "66"],
+    correct: "15",
+    questionId: "4811162"
+  },
+  {
+    question: "Which of the following is NOT a sister city of Tokyo?",
+    answers: ["Shanghai", "New York City", "Seoul", "London"],
+    correct: "Shanghai",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 10) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
